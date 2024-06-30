b928dc49d8fc2e7e4da8ba135640f2745777c8f9
white_lu <86720151+whiteluw@users.noreply.github.com>
2024-07-01 01:02:25 +0800

    更新 use.py

--- use.py
+++ use.py
@@ -4,6 +4,7 @@ import numpy as np
 import joblib
 from tkinter import Tk, filedialog
 from collections import Counter
+import time
 
 def load_image(filepath):
     if not os.path.exists(filepath):
@@ -23,25 +24,26 @@ def main():
         print("未选择任何模型文件。")
         return
 
-    image_file = filedialog.askopenfilename(title='选择要分类的图片', filetypes=[('图片文件', '*.png;*.jpg;*.jpeg')])
-    
-    if not image_file:
-        print("未选择任何图片文件。")
-        return
+    while True:
+        image_file = filedialog.askopenfilename(title='选择要分类的图片', filetypes=[('图片文件', '*.png;*.jpg;*.jpeg')])
+        
+        if not image_file:
+            return
 
-    try:
-        image = load_image(image_file)
-        image = np.array(image).reshape(1, -1)
-    except FileNotFoundError as e:
-        print(e)
-        return
+        try:
+            image = load_image(image_file)
+            image = np.array(image).reshape(1, -1)
+        except FileNotFoundError as e:
+            print(e)
+            return
 
-    predictions = []
-    for model_file in model_files:
-        knn = joblib.load(model_file)
-        label = knn.predict(image)
-        predictions.append(label[0])
-        print(f"模型 {model_file} 的预测结果：{label[0]}")
+        predictions = []
+        for model_file in model_files:
+            knn = joblib.load(model_file)
+            label = knn.predict(image)
+            predictions.append(label[0])
+            print(f"模型 {model_file} 的预测结果：{label[0]}")
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
